369802d3401349a0bf2d6fd8f029d9ed3d02c36c
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-12-09 21:38:47 +0300

    Performs many functions

--- multi_tool.sh
+++ multi_tool.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Default variables
 function="install"
-node="4132"
-rpc="3032"
+node="4133"
+rpc="3033"
 
 # Options
 . <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/colors.sh) --
@@ -17,15 +17,15 @@ while test $# -gt 0; do
 		echo -e "${C_LGn}Usage${RES}: script ${C_LGn}[OPTIONS]${RES}"
 		echo
 		echo -e "${C_LGn}Options${RES}:"
-		echo -e "  -h,  --help       show the help page"
-		echo -e "  -n,  --node PORT  assign the specified port to use RPC (default is ${C_LGn}${node}${RES})"
-		echo -e "  -r,  --rpc PORT   assign the specified port to use RPC (default is ${C_LGn}${rpc}${RES})"
-		echo -e "  -u,  --update     update the node"
-		echo -e "  -un, --uninstall  uninstall the node"
+		echo -e "  -h,   --help       show the help page"
+		echo -e "  -n,   --node PORT  assign the specified port to use RPC (default is ${C_LGn}${node}${RES})"
+		echo -e "  -r,   --rpc PORT   assign the specified port to use RPC (default is ${C_LGn}${rpc}${RES})"
+		echo -e "  -up,  --update     update the node"
+		echo -e "  -un, --uninstall   uninstall the node"
 		echo
 		echo -e "${C_LGn}Useful URLs${RES}:"
 		echo -e "https://github.com/SecorD0/Aleo/blob/main/multi_tool.sh — script URL"
-		echo -e "https://teletype.in/@letskynode/Aleo_RU — Russian-language guide"
+		echo -e "https://learning.1package.io/Aleo_RU — Russian-language guide"
 		echo -e "https://t.me/OnePackage — noderun and tech community"
 		echo
 		return 0 2>/dev/null; exit 0
@@ -40,7 +40,7 @@ while test $# -gt 0; do
 		rpc=`option_value $1`
 		shift
 		;;
-	-u|--update)
+	-up|--update)
 		function="update"
 		shift
 		;;
@@ -59,50 +59,49 @@ printf_n(){ printf "$1\n" "${@:2}"; }
 install() {
 	sudo apt update
 	sudo apt upgrade -y
-	sudo apt install tmux wget jq git build-essential pkg-config libssl-dev -y
+	sudo apt install wget jq git build-essential pkg-config libssl-dev -y
 	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/installers/rust.sh)
 	git clone https://github.com/AleoHQ/snarkOS.git --depth 1
 	cd snarkOS
 	cargo build --release
 	mv $HOME/snarkOS/target/release/snarkos /usr/bin
 	cd
-	if [ ! -f $HOME/account_aleo.txt ]; then
-		snarkos experimental new_account > $HOME/account_aleo.txt
+	if [ ! -f $HOME/aleo_account.txt ]; then
+		snarkos account new > $HOME/aleo_account.txt
 	fi
-	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n aleo_wallet_address -v `grep -oPm1 "(?<=Address  )([^%]+)(?=$)" $HOME/account_aleo.txt`
-	if [ ! -n "$aleo_wallet_address" ]; then
-		printf_n "${C_R}There is no \$aleo_wallet_address variable! \nCheck if the contents of the file are correct:${RES} cat $HOME/account_aleo.txt"
+	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n aleo_wallet_address -v `grep -oPm1 "(?<=Address  )([^%]+)(?=$)" $HOME/aleo_account.txt`
+	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n aleo_private_key -v `grep -oPm1 "(?<=Private Key  )([^%]+)(?=$)" $HOME/aleo_account.txt`
+	if [ ! -n "$aleo_private_key" ]; then
+		printf_n "${C_R}There is no \$aleo_private_key variable!\nCheck if the contents of the file are correct:${RES} cat $HOME/aleo_account.txt"
 		return 1 2>/dev/null; exit 1
 	fi
 	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/ports_opening.sh) "$node" "$rpc"
-	printf "[Unit]
+	sudo tee <<EOF >/dev/null /etc/systemd/system/aleod.service
+[Unit]
 Description=Aleo Miner
 After=network-online.target
 
 [Service]
 User=$USER
-ExecStart=`which snarkos` --miner ${aleo_wallet_address} --trial --node 0.0.0.0:${node} --rpc 0.0.0.0:${rpc}
+ExecStart=`which snarkos` start --nodisplay --prover $aleo_private_key
 Restart=on-failure
 RestartSec=3
 LimitNOFILE=65535
 
 [Install]
-WantedBy=multi-user.target" > /etc/systemd/system/aleod.service
+WantedBy=multi-user.target
+EOF
 	sudo systemctl daemon-reload
 	sudo systemctl enable aleod
 	sudo systemctl restart aleod
 	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n aleo_log -v "sudo journalctl -fn 100 -u aleod" -a
-	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/miscellaneous/insert_variable.sh) -n aleo_node_info -v ". <(wget -qO- https://raw.githubusercontent.com/SecorD0/Aleo/main/node_info.sh) -l RU 2> /dev/null" -a
 	printf_n "${C_LGn}Done!${RES}"
 	. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/logo.sh)
 	printf_n "
-The miner was ${C_LGn}started${RES}.
-Remember to save this file: ${C_LR}$HOME/account_aleo.txt${RES}
+The prover was ${C_LGn}started${RES}.
+Remember to save this file: ${C_LR}$HOME/aleo_account.txt${RES}
 
 \tv ${C_LGn}Useful commands${RES} v
-
-To view info about the node: ${C_LGn}aleo_node_info${RES}
-Page in a Checker: ${C_LGn}https://nodes.guru/aleo/aleochecker?q=`wget -qO- eth0.me`${RES}
 
 To view the node log: ${C_LGn}aleo_log${RES}
 To view the node status: ${C_LGn}sudo systemctl status aleod${RES}
@@ -112,8 +111,8 @@ To restart the node: ${C_LGn}sudo systemctl restart aleod${RES}
 update() {
 	if [ -f /etc/systemd/system/aleod.service ]; then
 		local service_file="aleod.service"
-	elif [ -f /etc/systemd/system/aleod-miner.service ]; then
-		local service_file="aleod-miner.service"
+	elif [ -f /etc/systemd/system/aleod-prover.service ]; then
+		local service_file="aleod-prover.service"
 	else
 		printf_n "${C_R}Change the name of the service file to ${C_LGn}aleod.service${RES}"
 		return 1 2>/dev/null; exit 1	
@@ -134,6 +133,7 @@ update() {
 		status=`/usr/bin/git pull`
 		if [ "$status" != "Already up to date." ]; then
 			printf_n "${C_LGn}Updating the node...!${RES}"
+			sudo systemctl stop "$service_file"
 			. <(wget -qO- https://raw.githubusercontent.com/SecorD0/utils/main/installers/rust.sh)
 			/root/.cargo/bin/cargo clean
 			/root/.cargo/bin/cargo build --release
